5f3d823e3e51207fbae9c58d6e356d67a59075f5
VictorFagund <fagundes.victor1604@gmail.com>
2024-06-19 23:34:53 -0300

    Rotas certas

--- routers/atividadeRouters.js
+++ routers/atividadeRouters.js
@@ -4,10 +4,9 @@ const authController = require('../controllers/authController');
 const authenticateToken = require('../middleware/authenticateToken');
 
 const router = express.Router();
-
-router.get('/atividadeList',authenticateToken, atividadeController.listarAtividades);
-router.post('/atividadeCreate',authenticateToken, atividadeController.criarAtividade);
-router.delete('/atividadeDelete/:id',authenticateToken, atividadeController.deletarAtividade);
-router.put('/atividadeUpdate/:id',authenticateToken, atividadeController.atualizarAtividade);
+router.get('/atividade',authenticateToken, atividadeController.listarAtividades);
+router.post('/atividade',authenticateToken, atividadeController.criarAtividade);
+router.delete('/atividade/:id',authenticateToken, atividadeController.deletarAtividade);
+router.put('/atividade/:id',authenticateToken, atividadeController.atualizarAtividade);
 
 module.exports = router;
--- routers/usuarioRouters.js
+++ routers/usuarioRouters.js
@@ -8,9 +8,9 @@ const router = express.Router();
 
 router.post('/login', authController.login);
 
-router.get('/usuarioList', authenticateToken, usuarioController.listarUsuarios);
-router.post('/usuarioCreate', usuarioController.criarUsuario);
-router.delete('/usuarioDelete/:id', authenticateToken, checkAdmin, usuarioController.deletarUsuario);
-router.put('/usuarioUpdate/:id', authenticateToken, usuarioController.atualizarUsuario);
+router.get('/usuario', authenticateToken, usuarioController.listarUsuarios);
+router.post('/usuario', usuarioController.criarUsuario);
+router.delete('/usuario/:id', authenticateToken, checkAdmin, usuarioController.deletarUsuario);
+router.put('/usuario/:id', authenticateToken, usuarioController.atualizarUsuario);
 
 module.exports = router;
